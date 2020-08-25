35548e8236ac850f622700920e89f74ea6e39e0f
ipazanin <ivan.pazanin1996@gmail.com>
2020-08-25 11:48:54 +0200

    Fixed graphql playground bug

--- source/Espresso.WebApi/Parameters/BodyParameters/GraphQLBodyParameters.cs
+++ source/Espresso.WebApi/Parameters/BodyParameters/GraphQLBodyParameters.cs
@@ -5,13 +5,13 @@
     /// </summary>
     public class GraphQLBodyParameters
     {
-        // public string? OperationName { get; set; }
-
         /// <summary>
         /// 
         /// </summary>
         /// <value></value>
-        public string? NamedQuery { get; set; }
+        public string? OperationName { get; set; }
+
+        // public string? NamedQuery { get; set; }
 
         /// <summary>
         /// 
