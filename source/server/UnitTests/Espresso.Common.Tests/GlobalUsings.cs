33f928411d66ded7294f429160d51590297e5265
ipazanin <ivan.pazanin1996@gmail.com>
2021-12-27 13:03:33 +0100

    Move GlobalUsings to each project

---
+++ source/server/UnitTests/Espresso.Common.Tests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// GlobalUsings.cs
+//
+// © 2021 Espresso News. All rights reserved.
+
+global using System;
+global using System.Collections.Generic;
+global using System.Text;
+global using System.Text.Json;
+global using System.Threading.Tasks;
---
+++ source/server/WebApi/Espresso.WebApi.Application/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿// GlobalUsings.cs
+//
+// © 2021 Espresso News. All rights reserved.
+
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
